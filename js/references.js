b53ed2efa685bee637b2a7d20196ab0e3bb4f2a6
Harold Garbeil <hg@Harolds-MBP.lan>
2024-12-13 15:38:43 -1000

    references started

---
+++ js/references.js
@@ -0,0 +1,25 @@
+
+let mainrefstring = `<section class="maincontent_top">
+    <div class="ref-container">
+    <h2>References and Resources</h2>
+    
+            <ul>
+                <li>United Nations, <a href="https://www.un.org/en/climatechange/what-is-climate-change" 
+                target="_blank">What is Climate Change? | United Nations, "https://www.un.org/en/climatechange/what-is-climate-change"</a> , accessed October 2024
+                </li>
+                
+                <li>NOAA Education, <a href="https://www.noaa.gov/education/resource-collections/climate/climate-change-impacts" target="_blank">NOAA Climate Change Impacts | NOAA Education, "https://www.noaa.gov/education/resource-collections/climate/climate-change-impacts"</a> , accessed October 2024<a>
+                </li>
+                
+                <li>Scripps Institute of Oceanography, UC San Diego , <a href="https://scrippsco2.ucsd.edu/" target="_blank">Scripps CO2 Program | Scripps Institute of Oceanography, "https://scrippsco2.ucsd.edu/"</a> , accessed October 2024<a>
+                </li>
+                <li>Scripps Institute of Oceanography, UC San Diego , <a href="https://scrippsco2.ucsd.edu/data/atmospheric_co2/primary_mlo_co2_record.html" target="_blank">Mauna Loa CO2 Data | Scripps CO2 Program | Scripps Institute of Oceanography, "https://scrippsco2.ucsd.edu/data/atmospheric_co2/primary_mlo_co2_record.html"</a> , accessed October 2024<a>
+               
+            </ul>
+    </div>
+    
+    </section>` ;
+            
+function loadReferences() {
+    mainEl.innerHTML=mainrefstring;
+}
